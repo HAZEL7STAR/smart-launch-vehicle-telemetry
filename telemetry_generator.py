930eedcbd24e0d81f4c3244a51d55943c0f4901b
HAZEL7STAR <hazel7star@gmail.com>
2026-08-04 10:12:21 +0530

    Built telemetry generator V1

--- telemetry_generator.py
+++ telemetry_generator.py
@@ -16,16 +16,60 @@ print(altitude)
 print(velocity)
 
 print("\nTelemetry Simulation Starts\n")
+status = "Normal"
 with open("telemetry.csv","w",newline="") as file:
     writer = csv.writer(file)
-
-    writer.writerow(["Time","Fuel","Altitude"])
+    
+    writer.writerow([
+        "Time",
+        "Temperature",
+        "Pressure",
+        "Fuel",
+        "Altitude",
+        "Velocity",
+        "Status"
+    ])
 
     for i in range(20):
-        print(f"Time:{time} Fuel:{fuel}% Altitude:{altitude}m")
+        if fuel <= 20:
+            status = "CRITICAL"
+        
+        elif fuel <= 50:
+            status = "LOW FUEL"
+            
+        else:
+            status = "NORMAL"
+        
+        print(
+            f"Time:{time} | "
+            f"Temp:{temperature}°C | "
+            f"Pressure:{pressure}kPa | "
+            f"Fuel:{fuel}% | "
+            f"Altitude:{altitude}m | "
+            f"Velocity:{velocity}m/s | "
+            f"Status:{status}"
+        )
 
-        writer.writerow([time, fuel, altitude])
+        writer.writerow([
+            time,
+            temperature,
+            pressure,
+            fuel,
+            altitude,
+            velocity,
+            status
+        ])
+        if fuel <= 50:
+            print("⚠ LOW FUEL WARNING")
 
         time += 1
+
         fuel -= 1
+
         altitude += random.randint(90,130)
+
+        velocity += random.randint(18,25)
+
+        temperature += random.randint(0,2)
+
+        pressure += random.randint(-1,1)
