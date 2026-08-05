11e4dcf1a35cab9e140f872d3087ddbb512bb033
HAZEL7STAR <hazel7star@gmail.com>
2026-08-05 06:50:16 +0530

    read a csv file

---
+++ telemetry_reader.py
@@ -0,0 +1,18 @@
+import csv
+
+with open("telemetry.csv", "r") as file:
+    reader = csv.reader(file)
+
+    next(reader)
+
+    for row in reader:
+        time = int(row[0])
+        fuel = int(row[3])
+        altitude = int(row[4])
+
+        print(time)
+        print(fuel)
+        print(altitude)
+        if fuel < 90:
+            print("Low Fuel")
+        print("------------")
